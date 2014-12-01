43ce0aebcfd827d76e075fd84c83b9badef9d0e7
Channing Walton <channingwalton@mac.com>
2014-12-01 22:23:54 +0000

    initial commit

---
+++ emacs.d/init.el
@@ -0,0 +1,177 @@
+;; This was shamelessly stolen from https://github.com/abedra/emacs.d/blob/master/abedra.org
+;; packages
+(require 'cl)
+(load "package")
+(package-initialize)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(setq package-archive-enable-alist '(("melpa" deft magit)))
+
+(defvar channing/packages '(auto-complete
+			    autopair
+			    company
+			    darcula-theme
+			    dash
+			    ensime
+			    epl
+			    expand-region
+			    f
+			    furl
+			    git-commit-mode
+			    git-rebase-mode
+			    haskell-mode
+			    htmlize
+			    magit
+			    markdown-mode
+			    marmalade
+			    org
+			    org-journal
+			    paredit
+			    popup
+			    projectile
+			    s
+			    sbt-mode
+			    scala-mode2
+			    smex
+			    tabbar
+			    tabbar-ruler
+			    web-mode
+			    writegood-mode
+			    yaml-mode)
+  "Default packages")
+
+(defun channing/packages-installed-p ()
+  (loop for pkg in channing/packages
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
+
+(unless (channing/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg channing/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+;; Marking text
+(delete-selection-mode t)
+(transient-mark-mode t)
+(setq x-select-enable-clipboard t)
+
+;; Display Settings
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b"))))
+
+(setq-default indicate-empty-lines t)
+(when (not indicate-empty-lines)
+  (toggle-indicate-empty-lines))
+
+;; Indentation
+(setq tab-width 2
+      indent-tabs-mode nil)
+
+;; yes and no
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Keybindings
+(global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-c C-k") 'compile)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; Misc
+(setq echo-keystrokes 0.1
+      use-dialog-box nil
+      visible-bell t)
+(show-paren-mode t)
+
+;; Smex
+(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; Ido
+(ido-mode t)
+(setq ido-enable-flex-matching t
+      ido-use-virtual-buffers t)
+
+;; autopair
+(require 'autopair)
+
+;; zsh
+(add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
+
+;; conf mode
+(add-to-list 'auto-mode-alist '("\\.gitconfig$" . conf-mode))
+
+;; projectile - https://github.com/bbatsov/projectile for keybindins
+(projectile-global-mode)
+(setq projectile-enable-caching t)
+
+;; tabbar
+(setq tabbar-ruler-global-tabbar t) ; If you want tabbar
+(setq tabbar-ruler-global-ruler t) ; if you want a global ruler
+(setq tabbar-ruler-popup-menu t) ; If you want a popup menu.
+(setq tabbar-ruler-popup-toolbar t) ; If you want a popup toolbar
+(setq tabbar-ruler-popup-scrollbar t) ; If you want to only show the
+                                        ; scroll bar when your mouse is moving.
+(require 'tabbar)
+
+;; org-mode and org-journal
+(require 'org)
+(setq org-directory "~/Dropbox/org/")
+(setq org-journal-dir "~/Dropbox/org/journal/")
+(setq org-agenda-files (list "~/Dropbox/org"
+                             "~/Dropbox/org/projects/boost" 
+                             "~/Dropbox/org/journal"))
+
+(setq org-journal-file-format "%Y%m%d.org")
+(setq org-log-done 'time)
+(setq org-startup-indented t)
+(setq org-ellipsis " \u25bc" )
+(setq org-completion-use-ido t)
+
+;; This from http://orgmode.org/worg/org-faq.html
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;; UI
+(if window-system
+    (require 'darcula-theme)
+  (load-theme 'wombat t))
+
+(set-face-attribute 'default nil :family "Input Mono Narrow")
+
+(set-face-attribute 'default nil :height 120)
+
+(setq pretty-symbol-categories '(lambda relational logical))
+
+(add-hook 'scala-mode 'pretty-symbols-mode)
+
+(add-to-list 'load-path "~/Code/dev/emacs-scalaz-unicode-input-method")
+(require 'scalaz-unicode-input-method)
+(add-hook 'scala-mode-hook 
+  (lambda () (set-input-method "scalaz-unicode")))
+
+(setq exec-path (append exec-path '("/Users/channing/Code/dev/sbt-extras/")))
+
+(add-hook 'scala-mode-hook (lambda () (setq truncate-lines t)))
+
+(electric-pair-mode 1)
+(column-number-mode 1)
+(desktop-save-mode 1) ; save/restore opened files
+
+;; keybindings
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;; Allow hash to be entered  
+(global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
