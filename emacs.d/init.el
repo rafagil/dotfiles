d4e8779c2b70bc6976477bf97aebd32b89fb82db
Channing Walton <channingwalton@mac.com>
2015-02-12 23:26:13 +0000

    modified some faces

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -241,6 +241,7 @@
 ;; Set up fonts
 (add-hook 'text-mode-hook 'variable-pitch-mode)
 (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-block nil :inherit 'fixed-pitch)
 (setq pretty-symbol-categories '(lambda relational logical))
 
 ;; Scala and Ensime
@@ -280,3 +281,15 @@
 ;; see http://stackoverflow.com/questions/2081577/setting-emacs-split-to-horizontal
 (setq split-height-threshold nil)
 (setq split-width-threshold 0)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-block ((t (:inherit shadow :foreground "SlateGray1")))))
