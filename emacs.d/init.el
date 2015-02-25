12946a91ce69785f572997d180314ab6972c5984
Channing Walton <channing.walton@barclays.com>
2015-02-25 09:12:41 +0000

    set the default font

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -67,7 +67,7 @@
 ;; Paths
 (setq exec-path (append exec-path '("/usr/local/bin/")))
 
-;; yas-snippet
+;; yasnippet
 (require 'yasnippet)
 (yas-reload-all)
 (add-hook 'prog-mode-hook
@@ -305,4 +305,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#a9b7c6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "nil" :family "Menlo"))))
  '(org-block ((t (:inherit shadow :foreground "SlateGray1")))))
