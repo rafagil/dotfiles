c90df463b22e59c46084176cf6792a05c05f391f
Channing Walton <channingwalton@mac.com>
2015-03-03 10:42:53 +0000

    minor tweaks and additions to the dictionary

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -326,7 +326,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(show-paren-mode t))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
