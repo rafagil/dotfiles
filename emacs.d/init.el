a2d1f44274a41197c992f19775f4ea28d5b96da9
Channing Walton <channing.walton@barclays.com>
2015-03-11 20:21:58 +0000

    fiddling with styles and snippets

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -105,6 +105,7 @@
 ;; Indentation
 (setq tab-width 2
       indent-tabs-mode nil)
+(setq-default indent-tabs-mode nil)
 
 ;; hide backup files etc
 (setq dired-omit-mode t)
@@ -331,6 +332,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(dired-listing-switches "-al")
  '(display-time-mode t)
  '(frame-background-mode (quote dark))
  '(show-paren-mode t)
@@ -340,4 +342,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default")))))
