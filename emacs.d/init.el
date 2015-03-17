647e326d065817ff39c43d25da277b478c9ea2d9
Channing Walton <channing.walton@barclays.com>
2015-03-17 14:19:32 +0000

    made TODO items top priority

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -186,7 +186,7 @@
 (define-key global-map "\C-cc" 'org-capture)
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline (concat org-directory "/todo.org") "Tasks")
-         "* TODO %U %?\n  %i\n  %a" :clock-in t :clock-resume t)
+         "* TODO [#A] %U %?\n  %i\n  %a" :clock-in t :clock-resume t)
         ("j" "Personal Journal" entry (file+datetree (concat org-directory "/journal.org"))
 	 "* %U\n\n%?\n")
 	("b" "Boost Journal" entry (file+datetree (concat org-directory "/projects/boost/journal-boost.org"))
