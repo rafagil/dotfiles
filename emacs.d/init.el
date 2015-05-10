19c7be2cf3547e5c309211fa81552f20ad7fa57e
Channing Walton <channingwalton@mac.com>
2015-05-10 22:44:02 +0100

    fiddling with fonts

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -486,4 +486,7 @@ is nil, refile in the current file."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-title ((t (:foreground "#de935f" :slant italic :weight bold :height 1.44))))
+ '(org-done ((t (:foreground "light green"))))
+ '(org-level-1 ((t (:inherit outline-1 :slant italic :height 1.4))))
+ '(org-level-2 ((t (:inherit outline-2 :slant italic :height 1.2)))))
