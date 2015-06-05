11a5e8b7a7eeb96a44235ad7790a4112e63a95a5
Channing Walton <channingwalton@mac.com>
2015-06-05 23:30:06 +0100

    improving the path

--- emacs.d/lisp/init-shell.el
+++ emacs.d/lisp/init-shell.el
@@ -1,6 +1,5 @@
 ;; shell set up
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
+(setenv "PATH" (shell-command-to-string "/bin/bash -c 'echo -n $PATH'"))
 
 ;; zsh
 (add-to-list 'auto-mode-alist '("\\.zsh$" . shell-script-mode))
