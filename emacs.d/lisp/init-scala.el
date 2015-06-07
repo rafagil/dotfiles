451572d81ed72d37d55d094bac7ffe0a648574ed
Channing Walton <channingwalton@mac.com>
2015-06-07 23:23:37 +0100

    more intellij keybindings

--- emacs.d/lisp/init-scala.el
+++ emacs.d/lisp/init-scala.el
@@ -15,13 +15,15 @@
 
 (defun scala-mode-intellij-keys ()
   "Modify keymaps used by `scala-mode'."
-  (local-set-key (kbd "s-n") 'ensime-search)
+  (local-set-key (kbd "s-n")    'ensime-search)
   (local-set-key (kbd "s-<f9>") 'ensime-typecheck-all)
-  (local-set-key (kbd "M-s-ø") 'ensime-refactor-organize-imports)
-  (local-set-key (kbd "M-s-¬") 'ensime-format-source)
+  (local-set-key (kbd "M-s-ø")  'ensime-refactor-organize-imports)
+  (local-set-key (kbd "M-s-¬")  'ensime-format-source)
   (local-set-key (kbd "S-<f6>") 'ensime-refactor-rename)
-  (local-set-key (kbd "M-s-√") 'ensime-refactor-extract-local)
-  (local-set-key (kbd "M-s-µ") 'ensime-refactor-extract-method)
+  (local-set-key (kbd "M-s-√")  'ensime-refactor-extract-local)
+  (local-set-key (kbd "M-s-µ")  'ensime-refactor-extract-method)
+  (local-set-key (kbd "M-s-~")  'ensime-refactor-inline-local)
+  (local-set-key (kbd "s-b")    'ensime-edit-definition)
   )
 
 (add-hook 'scala-mode-hook 'scala-mode-intellij-keys)
