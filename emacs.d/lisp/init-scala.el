31b64311fb00109e10d7a8c68ac64c4e5ab6e7a5
Channing Walton <channingwalton@mac.com>
2015-06-07 23:14:01 +0100

    adding intellij keybings for ensile

--- emacs.d/lisp/init-scala.el
+++ emacs.d/lisp/init-scala.el
@@ -13,4 +13,17 @@
 (add-hook 'scala-mode-hook (lambda () (setq indent-tabs-mode nil)))
 (add-hook 'scala-mode-hook (lambda () (setq show-trailing-whitespace t)))
 
+(defun scala-mode-intellij-keys ()
+  "Modify keymaps used by `scala-mode'."
+  (local-set-key (kbd "s-n") 'ensime-search)
+  (local-set-key (kbd "s-<f9>") 'ensime-typecheck-all)
+  (local-set-key (kbd "M-s-ø") 'ensime-refactor-organize-imports)
+  (local-set-key (kbd "M-s-¬") 'ensime-format-source)
+  (local-set-key (kbd "S-<f6>") 'ensime-refactor-rename)
+  (local-set-key (kbd "M-s-√") 'ensime-refactor-extract-local)
+  (local-set-key (kbd "M-s-µ") 'ensime-refactor-extract-method)
+  )
+
+(add-hook 'scala-mode-hook 'scala-mode-intellij-keys)
+
 (provide 'init-scala)
