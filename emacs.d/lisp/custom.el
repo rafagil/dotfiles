ad9b54244863d4fddbc64ebbde4a513b45d16a3a
Channing Walton <channingwalton@mac.com>
2015-06-18 23:32:04 +0100

    updating stuff

--- emacs.d/lisp/custom.el
+++ emacs.d/lisp/custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   (quote
+    (zenburn-theme yaml-mode writegood-mode web-mode tao-theme tabbar-ruler sublime-themes sr-speedbar smex sass-mode rainbow-mode paredit org-plus-contrib org-mac-iCal org-journal org monokai-theme marmalade markdown-mode magit less-css-mode latex-preview-pane journal ido-ubiquitous htmlize helm-swoop helm-projectile helm-ag helm-ack haskell-mode guide-key find-file-in-project f expand-region exec-path-from-shell ergoemacs-mode ensime darcula-theme color-theme-solarized color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized coffee-mode autopair))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
