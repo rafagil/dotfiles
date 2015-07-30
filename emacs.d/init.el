979ec85bf851a56e7410b126389ce1882747894d
Channing Walton <channing.walton@barclays.com>
2015-07-30 08:38:13 +0100

    tweaks

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -33,17 +33,4 @@
 (require 'init-keyboard)
 
 (require 'post-init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (git-commit-mode yaml-mode writegood-mode web-mode tabbar-ruler sr-speedbar smex rainbow-mode paredit org-plus-contrib org marmalade markdown-mode magit ibuffer-vc htmlize helm-swoop helm-projectile helm-ag haskell-mode guide-key f expand-region exec-path-from-shell ergoemacs-mode ensime darcula-theme color-theme-sanityinc-tomorrow autopair))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
