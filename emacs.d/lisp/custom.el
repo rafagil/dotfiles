3997035e2d05987e5c295dea7ba6fb05b98b0ea7
Channing Walton <channing.walton@barclays.com>
2015-08-06 08:24:47 +0100

    org bullets was causing issues

--- emacs.d/lisp/custom.el
+++ emacs.d/lisp/custom.el
@@ -3,9 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
+ '(package-selected-packages
    (quote
-    ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "118717ce0a2645a0cf240b044999f964577ee10137b1f992b09a317d5073c02d" default))))
+    (yaml-mode writegood-mode web-mode tabbar-ruler sr-speedbar spacemacs-theme smex rainbow-mode paredit org-plus-contrib org marmalade markdown-mode magit ibuffer-vc htmlize helm-swoop helm-projectile helm-ag haskell-mode guide-key f expand-region exec-path-from-shell ergoemacs-mode ensime darcula-theme color-theme-sanityinc-tomorrow avy autopair))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
