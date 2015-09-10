57c7987bec03d508ba43d1adcf60c3b549f11f76
Channing Walton <channing.walton@barclays.com>
2015-09-10 07:05:04 +0100

    Fixed the where Done goes

--- spacemacs-layers/my-org/packages.el
+++ spacemacs-layers/my-org/packages.el
@@ -67,7 +67,7 @@
               ("T" "Todo (Important)" entry (file+headline (concat org-directory "/todo.org") "To do")
                "* TODO [#A] %U %?\n  %i\n  %a")
 
-              ("d" "Done" entry (file+headline (concat org-directory "/todo.org") "Done")
+              ("d" "Todo" entry (file+headline (concat org-directory "/todo.org") "Done")
                "* DONE %U %?\n  %i\n")
 
               ("h" "HowTo" entry (file+headline (concat org-directory "/howto.org") "How To")
