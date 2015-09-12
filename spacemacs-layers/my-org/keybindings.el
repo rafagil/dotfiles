4701b9ddb4f551efb98b1eac25bfb6e4bf71cbc5
Channing Walton <channingwalton@mac.com>
2015-09-12 23:16:26 +0100

    added a keybinding

--- spacemacs-layers/my-org/keybindings.el
+++ spacemacs-layers/my-org/keybindings.el
@@ -1,4 +1,4 @@
 (evil-leader/set-key "oc" 'org-capture)
 (evil-leader/set-key "ol" 'org-store-link)
 (evil-leader/set-key "oa" 'org-agenda)
-
+(evil-leader/set-key "oj" 'org-clock-jump-to-current-clock)
