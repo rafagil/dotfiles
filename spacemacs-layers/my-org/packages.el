17b63043667de9d5ea110edae2cedb449de38ff0
Channing Walton <channing.walton@barclays.com>
2016-02-01 19:41:39 +0000

    automatic archival

--- spacemacs-layers/my-org/packages.el
+++ spacemacs-layers/my-org/packages.el
@@ -147,8 +147,14 @@
       (defun channing/clock-out-when-waiting ()
         (when (or (equal (org-get-todo-state) "WAITING") (equal (org-get-todo-state) "PAUSED")) (org-clock-out)))
 
+      (defun channing/archive-when-done ()
+        (when (equal (org-get-todo-state) "DONE") (org-archive-subtree-default)))
+
       (add-hook 'org-after-todo-state-change-hook
                 'channing/clock-out-when-waiting)
+
+      (add-hook 'org-after-todo-state-change-hook
+                'channing/archive-when-done)
 
       ;; Refiling
       (setq org-refile-targets '((org-agenda-files :maxlevel . 9)))
