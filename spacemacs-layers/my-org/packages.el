46bfb58959c3b96ab76d6c949cfca6d5a612e290
Channing Walton <channing.walton@barclays.com>
2016-02-08 13:25:56 +0000

    follow more is annoying

--- spacemacs-layers/my-org/packages.el
+++ spacemacs-layers/my-org/packages.el
@@ -66,7 +66,6 @@
                                    (concat project-directory "/boost")
                                    (concat project-directory "/foggyball")
                                    (concat org-directory "/blogs")))
-      (setq org-agenda-start-with-follow-mode t)
 
       ;; Clocking
       (setq org-clock-persist 'history)
