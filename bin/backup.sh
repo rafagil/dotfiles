850cbb63e4d3ae658a41b7d5aabb680bed5fde76
Channing Walton <channingwalton@mac.com>
2019-02-26 16:36:07 +0000

    updates

--- bin/backup.sh
+++ bin/backup.sh
@@ -3,6 +3,8 @@
 echo
 echo "**************** Backup starting at `date` ****************"
 
+echo "WARNING: Skipping SoF and CM"
+#/Users/channing/bin/backupSoF.sh
 /Users/channing/bin/backupRepos.sh
 /Users/channing/bin/mirror.sh
 
